d48549a2f00f5882313e2dcd11183d7c4082e4a6
bijaykumarpun <bijay.workplace@gmail.com>
2023-03-13 15:54:51 +0545

    Initial Commit

---
+++ Eatery/EateryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EateryApp.swift
+//  Eatery
+//
+//  Created by BIJAY on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct EateryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
